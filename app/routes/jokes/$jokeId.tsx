80c5e2fe70301cd999ab3129b6345207a54b178c
timotiusnc <timotius.nc@gmail.com>
2022-05-03 20:53:09 +0700

    Meta & Resource Routes (RSS)

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,4 +1,8 @@
-import type { ActionFunction, LoaderFunction } from "@remix-run/node";
+import type {
+  ActionFunction,
+  LoaderFunction,
+  MetaFunction,
+} from "@remix-run/node";
 import { redirect } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { db } from "~/utils/db.server";
@@ -6,6 +10,23 @@ import { db } from "~/utils/db.server";
 import type { Joke } from "@prisma/client";
 import { Link, useCatch, useLoaderData, useParams } from "@remix-run/react";
 import { getUserId, requireUserId } from "~/utils/session.server";
+
+export const meta: MetaFunction = ({
+  data,
+}: {
+  data: LoaderData | undefined;
+}) => {
+  if (!data) {
+    return {
+      title: "No joke",
+      description: "No joke found",
+    };
+  }
+  return {
+    title: `"${data.joke.name}" joke`,
+    description: `Enjoy the "${data.joke.name}" joke and much more`,
+  };
+};
 
 type LoaderData = { joke: Joke; isOwner: boolean };
 
